3fef784a4639e4203227d1f8949a7bdb9105b62f
ludwigschubi <ludwigschubi1@gmail.com>
2019-03-24 18:54:11 +0100

    Add create react app files and first component

---
+++ profile-viewer-react/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Profile from "./components/Profile";
+
+class App extends React.Component {
+  render() {
+    return (
+      <Profile/>
+    );
+  }
+}
+
+export default App;
