e6f345e23de2f67d3acedb529322f870119523bb
ludwigschubi <ludwigschubi1@gmail.com>
2019-03-24 19:27:00 +0100

    Add libraries to profile component

--- profile-viewer-react/src/components/Profile.js
+++ profile-viewer-react/src/components/Profile.js
@@ -2,9 +2,6 @@ import React from "react";
 import rdf from "rdflib";
 import auth from "solid-auth-client";
 
-const FOAF = new rdf.Namespace("http://xmlns.com/foaf/0.1/");
-const VCARD = new rdf.Namespace("http://www.w3.org/2006/vcard/ns#");
-
 class Profile extends React.Component {
     constructor(props){
         super(props)
