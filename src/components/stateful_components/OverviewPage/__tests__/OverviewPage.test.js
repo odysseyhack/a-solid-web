62d16c29ccc2cb5dea8b972598be8b9410257de4
Unknown <reg-2@t-online.de>
2019-04-02 19:37:23 +0200

    add tests to add and remove requests

--- src/components/stateful_components/OverviewPage/__tests__/OverviewPage.test.js
+++ src/components/stateful_components/OverviewPage/__tests__/OverviewPage.test.js
@@ -15,3 +15,42 @@ test("Request cards are displayed properly", () => {
   let page = component.toJSON();
   expect(page).toMatchSnapshot();
 });
+
+test("Add a request to the overview page", () => {
+  const component = renderer.create(<OverviewPage />);
+
+  let page = component.toJSON();
+  expect(page).toMatchSnapshot();
+
+  page.props.addRequest({
+    name: "Ludwig Schubert",
+    requests: ["View your comic collection", "Eat your food"]
+  });
+  page = component.toJSON();
+  expect(page).toMatchSnapshot();
+});
+
+test("Remove a request from the overview page", () => {
+  const component = renderer.create(
+    <OverviewPage
+      requests={Array(12).fill({
+        name: "Malte Sielski",
+        requests: ["View your phone number", "Edit your Birthdate"]
+      })}
+    />
+  );
+
+  let page = component.toJSON();
+  expect(page).toMatchSnapshot();
+
+  page.props.removeRequest(0);
+  page = component.toJSON();
+  expect(page).toMatchSnapshot();
+
+  page.props.removeRequest({
+    name: "Malte Sielski",
+    requests: ["View your phone number", "Edit your Birthdate"]
+  });
+  page = component.toJSON();
+  expect(page).toMatchSnapshot();
+});
