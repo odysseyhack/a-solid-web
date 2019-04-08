e72cd89795a7fc3795652b2289928bb119bb8d34
ludwigschubi <ludwigschubi1@gmail.com>
2019-04-08 14:52:48 +0200

    Add fetching method and pass down props

--- src/components/stateful_components/Navigation/Navigation.js
+++ src/components/stateful_components/Navigation/Navigation.js
@@ -1,37 +1,52 @@
 import React from "react";
 import rdf from "rdflib";
+import auth from "solid-auth-client";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import { NavLink } from "react-router-dom";
 import NavBarProfile from "../../functional_components/NavBarProfile/NavBarProfile";
 
 const FOAF = new rdf.Namespace("http://xmlns.com/foaf/0.1/");
+const VCARD = new rdf.Namespace("http://www.w3.org/2006/vcard/ns#");
 
 class Navigation extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      webId: undefined,
-      picture: "",
-      name: "",
+      picture: undefined,
+      name: undefined
     };
   }
 
-  fetchProfile(){
+  fetchProfile(webId) {
     const store = rdf.graph();
     const fetcher = new rdf.Fetcher(store);
 
-    const webId = this.state.webId;
-
-    if(webId){
-      fetcher.load(webId).then(()=> {
-        console.log("Loaded webId.")
+    if (webId) {
+      fetcher.load(webId).then(() => {
+        const picture = store.any(rdf.sym(webId), VCARD("hasPhoto"))
+        const pictureValue = picture 
+          ? picture.value
+          : undefined;
+        const name = store.any(rdf.sym(webId), FOAF("name"))
+        const nameValue = name
+          ? name.value
+          : undefined;
+        this.setState({
+          name: nameValue,
+          picture: pictureValue
+        });
+        console.log(this.state)
       });
     }
   }
 
-  componentDidMount(){
-    this.fetchProfile();
+  componentDidMount() {
+    auth.trackSession(session => {
+      if (session) {
+        this.fetchProfile(session.webId);
+      }
+    });
   }
 
   render() {
@@ -40,12 +55,14 @@ class Navigation extends React.Component {
         <Navbar bg="light" variant="light" fixed="top">
           <Navbar.Brand>
             <img src="favicon.ico" width="30" height="30" alt="Solid logo" />
-            <span style={{marginLeft: "5%"}}>Solid Web</span>
+            <span style={{ marginLeft: "5%" }}>Solid Web</span>
           </Navbar.Brand>
           <Nav className="mr-auto">
-            <NavLink to="/" style={{color: "#000", marginLeft: "10%"}}>Profile</NavLink>
+            <NavLink to="/" style={{ color: "#000", marginLeft: "10%" }}>
+              Profile
+            </NavLink>
           </Nav>
-          <NavBarProfile/>
+          <NavBarProfile picture={this.state.picture} name={this.state.name}/>
         </Navbar>
       </div>
     );
