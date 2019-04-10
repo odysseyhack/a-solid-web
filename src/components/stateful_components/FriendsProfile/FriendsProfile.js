644d3b03e70b6f823e357cdace14c46977a75cd3
ludwigschubi <ludwigschubi1@gmail.com>
2019-04-10 12:38:30 +0200

    Add initial friendsprofile component

---
+++ src/components/stateful_components/FriendsProfile/FriendsProfile.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class FriendsProfile extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            friendsWebId: ""
+        }
+    }
+
+    render(){
+        return <p>Hello World!</p>
+    }
+}
+
+export default FriendsProfile;
