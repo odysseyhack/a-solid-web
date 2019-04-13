9ec28ef43ca7ae9c8185a3adb8d62a1e5fb8e089
elliott <elliottbrunet@gmail.com>
2019-04-14 01:07:43 +0200

    image fix

--- src/components/ModalNotification/ModalNotificationView.js
+++ src/components/ModalNotification/ModalNotificationView.js
@@ -1,9 +1,7 @@
 import React from "react";
 import Modal from "react-bootstrap/Modal";
-import Button from "react-bootstrap/Button";
-import Iframe from 'react-iframe'
+import Iframe from 'react-iframe';
 
-//http://localhost:3006/
 class ModalNotificationView extends React.Component {
   render() {
     return (
@@ -11,27 +9,13 @@ class ModalNotificationView extends React.Component {
         {...this.props}
         size="lg"
         aria-labelledby="contained-modal-title-vcenter"
-        centered
-      >
-        {/* <iframe src="https://a-solid-web.github.io/profile-viewer-react/" target="_parent" /> */}
-
-        <Iframe url="http://localhost:3006/overview"
+        centered>
+        <Iframe url="https://a-solid-web.github.io/profile-viewer-react/overview"
         height="800px"
         width="800px"
-        // display="initial"
         position="relative"
         allowFullScreen
-        styles={{width: "100%", height: "100%", justifyContent:'center', alignItems:'center'}}
-        />
-        {/* <Modal.Header closeButton>
-          <Modal.Title id="contained-modal-title-vcenter">
-            Requested Permissions
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>hi</Modal.Body>
-        <Modal.Footer>
-          <Button onClick={this.props.onHide}>Close</Button>
-        </Modal.Footer> */}
+        styles={{width: "100%", height: "100%", justifyContent:'center', alignItems:'center'}}/>
       </Modal>
     );
   }
